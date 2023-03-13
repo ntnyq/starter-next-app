4e23c8d9430059e65a1dbbbc48c561419d682448
ntnyq <ntnyq13@gmail.com>
2023-03-13 14:04:07 +0800

    feat: add dynamic page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import '../css/tailwind.css'
-import { AppProps } from 'next/app'
+import '../styles/tailwind.css'
+import type { AppProps } from 'next/app'
 import { AnalyticsWrapper } from 'src/components/Analytics'
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Component {...pageProps} />
-    {process.env.NODE_ENV === 'production' && <AnalyticsWrapper />}
-  </>
-)
-
-export default App
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      {process.env.NODE_ENV === 'production' && <AnalyticsWrapper />}
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,40 @@
 import { NextSeo } from 'next-seo'
 import GithubCorner from 'react-github-corner'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import clsx from 'clsx'
 
-const Page = () => {
+export default function Index() {
+  const router = useRouter()
+  const { register, handleSubmit, setValue, setError, formState } = useForm({
+    defaultValues: {
+      name: '',
+    },
+  })
+  const onSubmit = handleSubmit(async values => {
+    if (!values.name) {
+      return setError('name', { message: 'Name is required' }, { shouldFocus: true })
+    }
+    router.push({ pathname: '/user/' + values.name })
+  })
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search)
+    const name = query.get('name')
+    if (name) {
+      setValue('name', name)
+      onSubmit()
+    }
+  }, [])
+
   return (
     <>
       <NextSeo
         title='starter-next-app'
         description='starter-next-app'
         openGraph={{
-          images: [
-            {
-              url: ``,
-            },
-          ],
+          images: [{ url: `` }],
         }}
         twitter={{
           cardType: 'summary_large_image',
@@ -23,17 +45,25 @@ const Page = () => {
           <h1 className='text-5xl font-bold'>starter-next-app</h1>
           <p className='text-zinc-500 text-sm mt-2'>hello next app</p>
         </header>
-        <form className='my-8'>
+        <form
+          onSubmit={onSubmit}
+          className='my-8'
+        >
           <div className='flex space-x-2'>
             <input
-              className='form-input w-full rounded-xl'
+              className={clsx(
+                'form-input w-full rounded-xl',
+                formState.dirtyFields.name && 'border-red',
+              )}
               placeholder='Input your name...'
               type='text'
+              {...register('name')}
             />
             <button
               className='shrink-0 px-5 h-12 rounded-xl bg-gradient-to-br from-gray-600 to-gray-700 text-white focus:ring-2 ring-indigo-400 ring-offset-2 inline-flex items-center disabled:opacity-50'
-              type='button'
+              type='submit'
               role='button'
+              disabled={formState.isSubmitting}
             >
               Go
             </button>
@@ -70,5 +100,3 @@ const Page = () => {
     </>
   )
 }
-
-export default Page
